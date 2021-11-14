5074fecce12ac6023e9f594014b5db150d52659b
LineofNile <paguio.jamesd@gmail.com>
2021-11-14 22:40:23 +0800

    StudentServiceiImplements

--- src/main/java/com/example/studentmanagement/demo/service/StudentServiceImpl.java
+++ src/main/java/com/example/studentmanagement/demo/service/StudentServiceImpl.java
@@ -1,4 +1,58 @@
-package com.example.studentmanagement.demo.service;
+package net.javaguides.springboot.service;
 
-public class StudentServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import com.example.studentmanagement.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.studentmanagement.demo.model.Student;
+import com.example.studentmanagement.demo.repository.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public List<Student> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public void saveStudent(Student student) {
+        this.studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(long id) {
+        Optional<Student> optional = studentRepository.findById(id);
+        Student student = null;
+        if (optional.isPresent()) {
+            student = optional.get();
+        } else {
+            throw new RuntimeException(" Student not found for id :: " + id);
+        }
+        return student;
+    }
+
+    @Override
+    public void deleteStudentById(long id) {
+        this.studentRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Student> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        return this.studentRepository.findAll(pageable);
+    }
 }
