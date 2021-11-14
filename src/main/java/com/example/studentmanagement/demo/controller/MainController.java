7a2ffccae4d141ff216c9d1d02f559c70fce0ffe
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-14 19:21:02 +0800

    Controller

--- src/main/java/com/example/studentmanagement/demo/controller/MainController.java
+++ src/main/java/com/example/studentmanagement/demo/controller/MainController.java
@@ -1,4 +1,12 @@
 package com.example.studentmanagement.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 public class MainController {
+    @GetMapping
+    public String login() {
+        return "login";
+    }
 }
