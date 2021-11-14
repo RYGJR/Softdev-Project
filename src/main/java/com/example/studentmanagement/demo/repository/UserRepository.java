b607723dc7cf7e034405f105f8ad7d9b1d95803d
LanceManganti <sparthenon19@gmail.com>
2021-11-14 20:27:10 +0800

    Update UserRepository.java

--- src/main/java/com/example/studentmanagement/demo/repository/UserRepository.java
+++ src/main/java/com/example/studentmanagement/demo/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.studentmanagement.demo.repository;
 
-public interface UserRepository {
+
+import com.example.studentmanagement.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    User findByEmail(String email);
 }
