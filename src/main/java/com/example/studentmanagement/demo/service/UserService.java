75b8686a47a386c1b0b207d2caea269b7ad33a23
LineofNile <paguio.jamesd@gmail.com>
2021-11-14 22:45:01 +0800

    Update UserService.java
    UserServiceUpdated

--- src/main/java/com/example/studentmanagement/demo/service/UserService.java
+++ src/main/java/com/example/studentmanagement/demo/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.studentmanagement.demo.service;
 
-public interface UserService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.example.studentmanagement.demo.dto.UserRegistrationDto;
+import com.example.studentmanagement.demo.model.User;
+
+public interface UserService extends UserDetailsService{
+    User save(UserRegistrationDto registrationDto);
 }
