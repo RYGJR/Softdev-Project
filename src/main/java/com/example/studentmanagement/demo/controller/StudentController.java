a6cf6c0fa72210c925f045f08d286cfb55d028c6
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-14 19:28:46 +0800

    Student Controller

--- src/main/java/com/example/studentmanagement/demo/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/demo/controller/StudentController.java
@@ -1,4 +1,75 @@
 package com.example.studentmanagement.demo.controller;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    // display list of students
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        return findPaginated(1, "firstName", "asc", model);
+    }
+
+    @GetMapping("/showNewStudentForm")
+    public String showNewStudentForm(Model model) {
+        // create model attribute to bind form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "new_student";
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        // save student to database
+        studentService.saveStudent(student);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable ( value = "id") long id, Model model) {
+
+        // get student from the service
+        Student student = studentService.getStudentById(id);
+
+        // set student as a model attribute to pre-populate the form
+        model.addAttribute("student", student);
+        return "update_student";
+    }
+
+    @GetMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable (value = "id") long id) {
+
+        // call delete student method
+        this.studentService.deleteStudentById(id);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+
+        Page<Student> page = studentService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List<Student> listStudents = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listStudents", listStudents);
+        return "index";
+    }
+
 }
