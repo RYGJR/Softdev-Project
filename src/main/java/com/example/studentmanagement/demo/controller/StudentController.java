2ed025d9ce8c5f565c402899048adaa5ad060421
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-14 19:29:42 +0800

    Student Service

--- src/main/java/com/example/studentmanagement/demo/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/demo/controller/StudentController.java
@@ -1,8 +1,19 @@
 package com.example.studentmanagement.demo.controller;
 
+import java.util.List;
 
+import com.example.studentmanagement.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.studentmanagement.demo.model.Student;
 @Controller
 public class StudentController {
 
@@ -51,7 +62,7 @@ public class StudentController {
 
 
     @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
                                 @RequestParam("sortField") String sortField,
                                 @RequestParam("sortDir") String sortDir,
                                 Model model) {
--- src/main/java/com/example/studentmanagement/demo/service/StudentService.java
+++ src/main/java/com/example/studentmanagement/demo/service/StudentService.java
@@ -1,4 +1,15 @@
 package com.example.studentmanagement.demo.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.studentmanagement.demo.model.Student
+
 public interface StudentService {
+    List<Student> getAllStudent();
+    void saveStudent(Student student);
+    Student getStudentById(long id);
+    void deleteStudentById(long id);
+    Page<Student> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
